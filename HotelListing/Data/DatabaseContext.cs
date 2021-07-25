71d173466ed1385b3115ef8c4d819d1212c19d75
gab <gabtan4749@gmail.com>
2021-07-25 15:01:21 +0800

    migrations

---
+++ HotelListing/Data/DatabaseContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext (DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country { CountryId = 1, Name = "Jamaica", ShortName = "JM" },
+                new Country { CountryId = 2, Name = "Bahamas", ShortName = "BS" },
+                new Country { CountryId = 3, Name = "Cayman Island", ShortName = "CI" }
+                );
+
+            builder.Entity<Hotel>().HasData(
+                new Hotel { HotelId = 1, Name = "Resort 1", Address = "Address 1", CountryId = 1, Rating = 1.1 },
+                new Hotel { HotelId = 2, Name = "Resort 2", Address = "Address 2", CountryId = 2, Rating = 2.5 },
+                new Hotel { HotelId = 3, Name = "Resort 3", Address = "Address 3", CountryId = 3, Rating = 3.4 }
+                );
+        }
+    }
+}
